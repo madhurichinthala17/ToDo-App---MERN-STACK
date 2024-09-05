53eef5c71a64711d704d585dc2cf28b6fd509a95
madhurichinthala17 <madhurichinthala2000@gmail.com>
2024-09-04 23:00:19 -0400

    ToDO App

---
+++ frontend/src/utils/HandleApi.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+const baseUrl="http://localhost:5000"
+
+const getAllToDo=(setToDo) => {
+     axios.get(baseUrl)
+     .then(({data}) => {
+        console.log('data --->',data);
+        setToDo(data)
+     })
+}
+
+const addToDo=(text,setText,setToDo) => {
+
+    axios.post(`${baseUrl}/save`,{text})
+    .then((data) => {
+        console.log(data);
+        setText("")
+        getAllToDo(setToDo)
+    })
+    .catch((err) => console.log(err))
+}
+
+const updateToDo=(toDoId,text,setText,setToDo,setIsUpdating) => {
+
+    axios.post(`${baseUrl}/update`,{_id:toDoId,text}).then((data) => {
+        setText("")
+        setIsUpdating(false)
+        getAllToDo(setToDo)
+    })
+    .catch((err) => console.log(err))
+}
+
+const deleteToDo=(_id,setToDo) => {
+
+    axios.delete(`${baseUrl}/delete`,{data: {_id}}).then((res) => {
+        console.log("Delete api successfull")
+        getAllToDo(setToDo)
+    })
+    .catch((err) => console.log(err))
+}
+
+export { addToDo, deleteToDo, getAllToDo, updateToDo };
+
