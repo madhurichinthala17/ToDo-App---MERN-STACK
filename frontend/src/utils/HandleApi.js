e57d842884793150d2975522163192aa6b117402
madhurichinthala17 <madhurichinthala2000@gmail.com>
2024-09-05 20:41:28 -0400

    Adding render

--- frontend/src/utils/HandleApi.js
+++ frontend/src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl="http://localhost:5000"
+const baseUrl="https://todo-app-mern-stack-y6b4.onrender.com"
 
 const getAllToDo=(setToDo) => {
      axios.get(baseUrl)
